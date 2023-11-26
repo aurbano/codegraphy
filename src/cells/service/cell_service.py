1f0a1c9e202c77d81c2855f3cda8b0372116453c
Alejandro U. Alvarez <aluralma@gmail.com>
2023-11-26 23:23:02 +0800

    moved graph service to private

--- src/cells/service/cell_service.py
+++ src/cells/service/cell_service.py
@@ -2,11 +2,10 @@ from src.graphs.service.graph_service import GraphService
 
 
 class CellService:
-    graph_service = GraphService()
-    # TODO: Add a graph validator (outputs, params, links, repeated ids... )
+    _graph_service = GraphService()
 
     def read_cell_contents(self, file_path: str, cell_id: str) -> str:
-        graph = self.graph_service.read_graph(file_path)
+        graph = self._graph_service.read_graph(file_path)
         cell = [cell for cell in graph.cells if cell.id == cell_id]
 
         if len(cell) != 1:
